7621e12e6a626e7cc0c7dee16cd3561c484f7484
Andrey Schneider <andrey@oneweb.com.br>
2022-06-24 08:58:03 -0300

    Debugando o index.php

--- Cadastro/index.php
+++ Cadastro/index.php
@@ -1,9 +1,16 @@
 <?php
+
+    $username = $_POST["userlogin"];
+    $fullname = $_POST["nomecompleto"];
+    $document = $_POST["cpf"];
+    $email = $_POST["email"];
+
+
     $user = array(
-        "username" => isset($_POST["userlogin"])?$_POST["userlogin"]:"[ERRO! USUAŔIO NÃO INFORMADO!]",
-        "fullname" => isset($_POST["nomecompleto"])?$_POST["nomecompleto"]:"[ERRO! NOME COMPLETO NÃO INFORMADO!]",
-        "document" => isset($_POST["cpf"])?$_POST["cpf"]:"[ERRO! CPF NÃO INFORMADO!]",
-        "email" => isset($_POST["email"])?$_POST["email"]:"[ERRO! EMAIL NÃO INFORMADO!]"
+        "username" => $username,
+        "fullname" => $fullname,
+        "document" => $document,
+        "email" => $email
     );
 
     print_r($user);
